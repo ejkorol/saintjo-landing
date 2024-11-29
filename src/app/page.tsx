c12c891ab79bbf263448e39326998daaccc56b25
Jason Korol <ejkorol@gmail.com>
2024-11-29 17:48:31 -0500

    updated font sizes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ const Home = () => {
       <section className="flex flex-col items-center justify-center h-svh w-full backdrop-brightness-50">
         <Image
           src="/logos/logo.png"
-          className="absolute top-0 mt-16"
+          className="absolute top-0 mt-16 w-[173px] md:h-[45px] md:w-[346px]"
           alt="saint jo"
           width={346}
           height={45}
         />
-        <h1 className="font-americana uppercase text-8xl text-white tracking-widest">
+        <h1 className="font-americana uppercase text-4xl md:text-8xl text-white tracking-widest">
           coming soon
         </h1>
       </section>
